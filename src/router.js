238048eab5d6442ab42130d40304192f5cc42c11
michael <michael.gallipo@gmail.com>
2021-07-04 17:50:29 -0400

    Initial Commit

--- src/router.js
+++ src/router.js
@@ -14,13 +14,19 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/activities",
+      name: "activities",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/Activities.vue")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () =>
+        import("./views/Add.vue")
     }
   ]
 });
